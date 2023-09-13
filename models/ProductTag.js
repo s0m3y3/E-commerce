deb82c1e14b183cf81e2b57d648298d9c3617f7e
Yee Y <s0m3y3@gmail.com>
2023-09-12 19:28:12 -0500

    completed models folder's todo list. updated the proper columns.

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,24 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        product_id: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        tag_id: 'id',
+      },
+    },
   },
   {
     sequelize,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,39 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const driverData = await Driver.findAll({
+      include: [{ model: License }, { model: Car }],
+    });
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+
+  try {
+    const driverData = await Driver.findByPk(req.params.id, {
+      include: [{ model: License }, { model: Car }],
+    });
+
+    if (!driverData) {
+      res.status(404).json({ message: 'No driver found with that id!' });
+      return;
+    }
+
+    res.status(200).json(driverData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
 });
 
 router.post('/', (req, res) => {
